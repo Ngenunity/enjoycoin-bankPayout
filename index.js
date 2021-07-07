66e1064024b074eb6d6885d9a1ad7d3de715953b
calaunte <calaunte@protonmail.com>
2021-07-07 16:53:46 -0400

    removed masquerade

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context, callback) => {
 
       // Transfer ETH to destination
       const timestamp = new Date().getTime();
-      const transferUrl = `https://api.sendwyre.com/v3/transfers?masqueradeAs=${wyreId}&timestamp=${timestamp}`;
+      const transferUrl = `https://api.sendwyre.com/v3/transfers?timestamp=${timestamp}`;
 
       // Calculate request signature
       const signature = (url, data) => {
